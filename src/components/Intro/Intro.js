3f5545dedf9feb89333471723025f7ed2c8c38fe
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-28 15:53:57 +0100

    Update Intro.js

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -2,7 +2,7 @@ import {useEffect} from 'react';
 
 import './Intro.scss';
 
-import intro_video from '../../assets/videos/web main page.webm';
+import intro_video from '../../assets/videos/web main page_1 (1).mp4';
 import mobile_video from '../../assets/videos/phone first page.webm';
 import bottom from '../../assets/img/introBottom.svg';
 

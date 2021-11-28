322b134b9cea8c2cc9252b545f8d6509cdb3109e
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-28 06:19:33 +0100

    Update Galaxy.js

--- src/components/Galaxy/Galaxy.js
+++ src/components/Galaxy/Galaxy.js
@@ -1,5 +1,5 @@
 import './Galaxy.scss';
-import animation from '../../assets/Roadmap/100.webm';
+import animation from '../../assets/videos/Roadmap/100.webm';
 
 export const Galaxy = () => {
     return (

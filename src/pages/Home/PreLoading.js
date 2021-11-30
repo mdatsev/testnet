6bb872c94c54df4c776a44099f135b24c0708b25
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 21:08:56 +0100

    Update PreLoading.js

--- src/pages/Home/PreLoading.js
+++ src/pages/Home/PreLoading.js
@@ -3,17 +3,21 @@ import side1 from '../../assets/img/roadmap/Game.mp4';
   const Loading = () =>{
     return (
     
-   <div align='center'>
-            <animation
-                type="Hearts"
-                color="rgb(0, 153, 255)"
-                height={100}
-                width={100}
-                timeout={3000} //3 secs
-            />
-        </div>
+ <h2>sadsadasdasdasdass
+      sadsadasdasdasdassadsadasdasdasdassadsadasdasdasdassadsadasdasdasdas
+      sadsadasdasdasdassadsadasdasdasdassadsadasdasdasdas
+      sadsadasdasdasdas
+      sadsadasdasdasdassadsadasdasdasdasadsadasdasdasdassadsadasdasdasdassadsadasdasdasdassadsadasdasdasdas </h2>
+   //  <div align='center'>
+     //       <animation
+       //         type="Hearts"
+         //       color="rgb(0, 153, 255)"
+           //     height={100}
+            //    width={100}
+             //   timeout={3000} //3 secs
+           // />
+        //</div>
 
-   
   
     )
   }

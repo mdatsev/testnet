042922ef16868d51a64789efb11319be1d277d1d
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 22:59:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ useEffect(() => {
 	  {isLoading && (
 	  
 	  
-	<div  align="center" className="center">
+	<div style={{backgroundColor:'#090'}} align="center" className="center">
 	  <img style={{width:'250px' , height:'250px'}} src={logo}/>
 	
 	  </div>

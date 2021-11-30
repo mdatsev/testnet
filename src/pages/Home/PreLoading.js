59beec2e14367b91c37848abdbff699ebeb2ecea
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 21:11:51 +0100

    Update PreLoading.js

--- src/pages/Home/PreLoading.js
+++ src/pages/Home/PreLoading.js
@@ -1,20 +1,15 @@
 import animation from '../../assets/img/team/Monir.png';
 import side1 from '../../assets/img/roadmap/Game.mp4';
-import Home from './pages/Home/Home';
-
   const Loading = () =>{
     return (
-    <Home /> 
-
-   //  <div align='center'>
-     //       <animation
-       //         type="Hearts"
-         //       color="rgb(0, 153, 255)"
-           //     height={100}
-            //    width={100}
-             //   timeout={3000} //3 secs
-           // />
-        //</div>
+  
+     <div align='center'>
+            <side1
+                height={100}
+                width={100}
+                timeout={3000} //3 secs
+          />
+        </div>
 
   
     )

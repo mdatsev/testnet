f5bdc184a68167a3600a43f6e63b8e6e1a2ba9af
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 21:07:36 +0100

    Update PreLoading.js

--- src/pages/Home/PreLoading.js
+++ src/pages/Home/PreLoading.js
@@ -2,9 +2,17 @@ import animation from '../../assets/img/team/Monir.png';
 import side1 from '../../assets/img/roadmap/Game.mp4';
   const Loading = () =>{
     return (
-     <div align='center'>
-    <h2 style={{ color: 'red' }}> dsgfdsgdgs</h2> 
-      </div>
+    
+   <div align='center'>
+            <animation
+                type="Hearts"
+                color="rgb(0, 153, 255)"
+                height={100}
+                width={100}
+                timeout={3000} //3 secs
+            />
+        </div>
+
    
   
     )

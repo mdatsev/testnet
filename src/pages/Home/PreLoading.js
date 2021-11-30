9f6be70bb11cb937e952e6953bb4690b8d10a4d0
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 20:48:35 +0100

    Update PreLoading.js

--- src/pages/Home/PreLoading.js
+++ src/pages/Home/PreLoading.js
@@ -3,8 +3,6 @@ import animation from '../../assets/videos/loading (1).webm';
     return (
       <div align='center'>
       <animation
-        type="Puff"
-        color="#00BFFF"
         height={100}
         width={100}
         timeout={3000} //3 secs

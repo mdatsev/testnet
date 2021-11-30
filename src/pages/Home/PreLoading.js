c51fbf99dbedda1904237a4f05df16eaa3c93459
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 20:32:18 +0100

    Update PreLoading.js

--- src/pages/Home/PreLoading.js
+++ src/pages/Home/PreLoading.js
@@ -1,26 +1,19 @@
-import "react-loader-spinner/dist/loader/css/react-spinner-loadercss"
-import Loader from "react-loader-spinner"
-import React from 'react';
-const Load = () => {
+import React from 'react'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from 'react-loader-spinner'
+
+const Loading = () => {
     return (
-
-
         <div align='center'>
-        <Loader 
-        type ="Puff"
-        color="rgb(0,153,255)"
-        height ={100}
-        width ={100}
-        timeout={3000}
-/>
-            
+            <Loader
+                type="Hearts"
+                color="rgb(0, 153, 255)"
+                height={100}
+                width={100}
+                timeout={3000} //3 secs
+            />
         </div>
-                )
-                
-                
-                
-                }
-                
-                
-                
-export default Load;
+    )
+}
+
+export default Loading

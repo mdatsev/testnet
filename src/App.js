7c415d665f0437bf79ced34827222f997888c9d0
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 20:13:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './App.scss';
 import Home from './pages/Home/Home';
 
 function App() {
+[isLoading, setIsLoading ] = useState(true);
+useEffect(() =>{
+	effect
+	return() =>{
+		cleanup
+	}
+})
   return (
     <div className="App">
 		<Home />

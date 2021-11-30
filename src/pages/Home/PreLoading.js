8874414e435f94f36dcdea81412bea518e40fd04
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 20:58:29 +0100

    Update PreLoading.js

--- src/pages/Home/PreLoading.js
+++ src/pages/Home/PreLoading.js
@@ -1,13 +1,17 @@
 import animation from '../../assets/img/team/Monir.png';
+import side1 from '../../assets/img/roadmap/Game.mp4';
   const Loading = () =>{
     return (
-      <div align='center'>
-      <animation
+      <video autoPlay loop muted width="330" height="330" alt="pic" src={side1}>
+      
+         <animation
         height={100}
         width={100}
         timeout={3000} //3 secs
       />
-          </div>
+      </video>
+   
+
     )
   }
 

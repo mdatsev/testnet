bd6638d88833cb887e9b6418007e5ee1a5a18efd
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 21:13:32 +0100

    Update PreLoading.js

--- src/pages/Home/PreLoading.js
+++ src/pages/Home/PreLoading.js
@@ -4,7 +4,7 @@ import side1 from '../../assets/img/roadmap/Game.mp4';
     return (
   
      <div className="home">
-            <side1
+            <animation
                 height={100}
                 width={100}
                 timeout={3000} //3 secs

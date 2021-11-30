7d189290b48cec44a7e50e65c87ed4a054cbf474
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 20:14:08 +0100

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,21 +9,9 @@ import Roadmap from '../../components/Roadmap/Roadmap';
 import Team from '../../components/Team/Team';
 import FaqComponent from '../../components/Faq/Faq';
 import Footer from '../../components/Footer/Footer';
-import "react-loader-spinner/dist/loader/css/react-spinner-loadercss"
-import Loader from "react-loader-spinner"
+
 const Home = () => {
     return (
-
-
-        <div align='center'>
-        <Loader 
-        type ="Puff"
-        color="rgb(0,153,255)"
-        height ={100}
-        width ={100}
-        timeout={3000}
-        </div>
-                
 
 <div className="home" >
 

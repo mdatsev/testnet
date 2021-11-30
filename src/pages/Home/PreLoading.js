a795eb8e4b789c357ba061db69e60c5030163a23
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 21:02:13 +0100

    Update PreLoading.js

--- src/pages/Home/PreLoading.js
+++ src/pages/Home/PreLoading.js
@@ -3,15 +3,7 @@ import side1 from '../../assets/img/roadmap/Game.mp4';
   const Loading = () =>{
     return (
      <div align='center'>
-       <video autoPlay loop muted width="330" height="330" alt="pic" src={side1}>
-      
-         <animation
-        height={100}
-        width={100}
-        timeout={3000} //3 secs
-      />
-      </video>
-      
+    <h2 style={{ color: 'red' }}> dsgfdsgdgs</h2> 
       </div>
    
   

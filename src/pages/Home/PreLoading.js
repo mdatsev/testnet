9b1c193801df4aa8a9207e4c059bfeb044214a76
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-11-30 20:14:41 +0100

    Create PreLoading.js

---
+++ src/pages/Home/PreLoading.js
@@ -0,0 +1,22 @@
+import "react-loader-spinner/dist/loader/css/react-spinner-loadercss"
+import Loader from "react-loader-spinner"
+const Home = () => {
+    return (
+
+
+        <div align='center'>
+        <Loader 
+        type ="Puff"
+        color="rgb(0,153,255)"
+        height ={100}
+        width ={100}
+        timeout={3000}
+        </div>
+                )
+                
+                
+                
+                }
+                
+                
+                

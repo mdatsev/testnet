c7dec88058c4366fd49641a41c706af3837f7fdc
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-12-03 20:17:04 +0100

    Update Vision.js

--- src/components/Vision/Vision.js
+++ src/components/Vision/Vision.js
@@ -16,7 +16,7 @@ export const Vision = () => {
                 <div className="vision__content__desc">
                     <p>
                     Talented people should always be able to show the world what they are capable of. Our mission is to make a voice for whoever needs a push in their journey, That's our main goal. <br/><br/>
-                    Buff Doges are a collection of 10,000 unique generative characters precisely designed, but they are not alone! Every planet will have its own collection, where they face each other in the Galaxy War game, where our community will be able to access the metaverse and determine their winners while earning money playing their favorite game.
+                    Buff Doges are a collection of 5555 unique generative characters precisely designed, but they are not alone! Every planet will have its own collection, where they face each other in the Galaxy War game, where our community will be able to access the metaverse and determine their winners while earning money playing their favorite game.
                     </p>
                 </div>
             </div>

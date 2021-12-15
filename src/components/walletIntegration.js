4e943b6f6e41fb47e1d284e720a154c3f8f441b3
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-12-15 02:16:41 +0100

    Update walletIntegration.js

--- src/components/walletIntegration.js
+++ src/components/walletIntegration.js
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 
 const config = {
-  contractAddress: '0x978b545a0d0A18DD5F961493bF484593a1cA34aB',
+  contractAddress: '0x7EA71b8C3f1fbB2EC9762CEd93f955d5fc512b92',
   // networkName: 'Ethereum Mainnet',
   networkName: 'Ethereum Testnet Rinkeby',
   // etherScanUrl: 'https://etherscan.io/tx/',

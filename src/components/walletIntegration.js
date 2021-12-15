ed203791cad98b798e6b1f4dd5047554a2fe023c
1Alpha10 <92266394+1Alpha10@users.noreply.github.com>
2021-12-15 21:25:43 +0100

    Update walletIntegration.js

--- src/components/walletIntegration.js
+++ src/components/walletIntegration.js
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 
 const config = {
-  contractAddress: '<PUT MAINNET ADDRESS HERE>',
+  contractAddress: '<0xFe5737bE28af719A9fcD8abf37F52234dD6cBB8F>',
   networkName: 'Ethereum Mainnet',
   etherScanUrl: 'https://etherscan.io/tx/',
   openSeaUrl: 'https://opensea.io/account?search[sortBy]=CREATED_DATE&search[sortAscending]=false',
